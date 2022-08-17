c1ea0d8f47d697cce4bfd9a35d746e1f0a6f52a9
HwangJunha <45688526+HwangJunha@users.noreply.github.com>
2022-08-17 23:47:33 +0900

    [master] 정적 리소스 지원 및 경로 설정

--- src/main/java/com/around/springmvc/config/WebConfig.java
+++ src/main/java/com/around/springmvc/config/WebConfig.java
@@ -2,9 +2,17 @@ package com.around.springmvc.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 //@EnableWebMvc //전부 사라지고 처음부터 다시 설정해줘야 한다
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/m/**")
+                .addResourceLocations("classpath:/m/") //반드시 /로 끝나야 한다
+                .setCachePeriod(20); //초 단위 캐시 컨트롤
+    }
 }
